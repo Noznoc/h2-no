1f69dbb9f9b2a437d6a21f9406c68a28312fa133
Noznoc <julia.conzon@mapbox.com>
2017-11-22 08:51:58 -0500

    adjusted data-driven styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onload = function() {init()}; // Initialize TableTop on window load
+window.onload = function() {init()}; // initialize TableTop on window load
 			
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1IKvJUsED-w9hAmnbId3OqVCqRS_jMgyr-2rXaZ8L7mY/edit?usp=sharing';
 
@@ -8,10 +8,13 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/julconz/cja00smvy92252sob26ardnrv',
   center: [-99.93, 54.32],
-  zoom: 4
+  zoom: 4,
+  maxZoom: 10,
+  minZoom: 3,
+  maxBounds: [-168.39312,40.713956,-50.971241,83.359511]
 });
 
-function init() { // Function that initializes TableTop. Tabletop will pull the data from the Google Sheet that stores all the da
+function init() { // function that initializes TableTop. Tabletop will pull the data from the Google Sheet that stores all the da
 	Tabletop.init( { 
 		key: public_spreadsheet_url,
 		callback: showInfo,
@@ -22,16 +25,13 @@ function init() { // Function that initializes TableTop. Tabletop will pull the 
 var features = [];
 var years = [];
 
-function showInfo(data, tabletop) { // Function to show data from Google Sheet
-	
+function showInfo(data, tabletop) { // function to show data from Google Sheet
 	data.forEach(function(data) {
 		if (data.lng) {
 			// store all lat, lng as geojson features
 			var coordinates = [parseFloat(data.lng), parseFloat(data.lat)];
 			var year = parseInt(data.date_set.substr(0, 4));
-			
-			features.push(JSON.parse('{"type":"Feature","properties":{"description":"' + data.community + '","date_revoked":"' + data.date_revoked + '","population":' + parseInt(data.population_int) + ',"year":' + year + ',"date_set":"' + data.date_set + '"},"geometry":{"type":"Point","coordinates":[' +  coordinates + ']}}'));
-
+			features.push(JSON.parse('{"type":"Feature","properties":{"description":"' + data.community + '","date_revoked":"' + data.date_revoked + '","advisory_type":"' + data.advisory_type + '","year":' + year + ',"date_set":"' + data.date_set + '","system_name":"' + toString(data.system_name) + '"},"geometry":{"type":"Point","coordinates":[' +  coordinates + ']}}'));
 			// store all years
 			if (years.indexOf(year) == -1){
 				years.push(year);
@@ -42,20 +42,21 @@ function showInfo(data, tabletop) { // Function to show data from Google Sheet
 	// sort years
 	years.sort(function(a, b){return a-b})
 
-	// load map
-	renderMap(features);
+	map.on('load', function(){
+		// load map
+		renderMap(features);
+	})
 
 	// filter by time
 	function filterBy(year, total){
 		var filters = ['<=', 'year', year];
 		map.setFilter('water-advisories', filters);
-		document.getElementById('year').textContent = 'Since ' + year;
+		document.getElementById('year').textContent = 'Recorded advisories since ' + year;
 		if (total == 1) {
 			document.getElementById('stats').innerHTML = total + ' advisory in ' + year;
 		} else {
 			document.getElementById('stats').innerHTML = total + ' advisories in ' + year;
 		}
-
 	}
 
 	// render map
@@ -87,26 +88,33 @@ function showInfo(data, tabletop) { // Function to show data from Google Sheet
 			type: 'circle',
 			source: 'markers',
 			paint: {
-				'circle-color': {
+				'circle-stroke-color': {
 					property: 'date_revoked',
-					default: '#252726',
+					default: '#1494A8',
 					type: 'categorical',
 					stops: [
 						['None', '#e04763'],
 					]
-				}/*, 
-				'circle-radius': {
-					property: 'population',
-					type: 'interval',
+				},
+				'circle-color': {
+					property: 'advisory_type',
+					type: 'categorical',
+					default: '#e04763',
 					stops: [
-						[5, 3],
-						[10, 4],
-						[20, 5],
-						[30, 6],
-						[40, 7],
-						[50, 8]
+						['BWA', '#C6BDB0'],
+						['DNC', '#635F58'],
+						['BWO', '#C6BDB0']
 					]
-				}*/
+				},
+				'circle-stroke-width': {
+					property: 'advisory_type',
+					type: 'categorical',
+					default: 3,
+					stops: [
+						['BWA', 3],
+						['DNC', 3]
+					]
+				}
 			}
 		}, labels);
 
@@ -123,7 +131,6 @@ function showInfo(data, tabletop) { // Function to show data from Google Sheet
 		map.on('mouseenter', 'water-advisories', function(e) {
 			var text;
 			map.getCanvas().style.cursor = 'pointer';
-
 			// set popup text
 			if (e.features[0].properties.date_revoked !== 'None'){
 				text = '<h2>' + e.features[0].properties.description + '</h2><h3>Date Set: ' + e.features[0].properties.date_set + '<br>Date Revoked: ' + e.features[0].properties.date_revoked + '</h3>';
